28294cd46596f9d3b5f54037962ed8cd9634b61d
Andrew Deir <andrew.deir@cis.edu.sg>
2017-12-06 16:13:35 +0800

    Update Solar_Water_System.ino

--- Solar_Water_System.ino
+++ Solar_Water_System.ino
@@ -6,26 +6,26 @@ int M2 = 7;
 int soilSensor = 0;
 
 //moisture variables
-int moistureThreshold = 300;
+int moistureThreshold = 300;//calibrate this variable based on how damp the soil needs to be for the particular plant
 int waitTime = 5000; //ms
 int waterTime = 1000; //ms
 
  
 void setup() 
 { 
-    pinMode(M1, OUTPUT);
-    pinMode(A0, INPUT); 
-    Serial.begin(57600);
+    pinMode(M1, OUTPUT);//motor 1
+    pinMode(A0, INPUT); //moisture sensor input
+    Serial.begin(57600);//comment out after calibration
 } 
  
 void loop() 
 { 
  
 soilSensor = analogRead(A0);
-Serial.println(soilSensor); //use for calibration
+Serial.println(soilSensor); //use for calibration, then comment out
 if (soilSensor < moistureThreshold) {
-  digitalWrite(M1, HIGH);
-  analogWrite(E1, 255);
+  digitalWrite(M1, HIGH);//turn motor 1 one
+  analogWrite(E1, 255);//PWM speed control
   delay(waterTime);
   digitalWrite(M1, LOW);
   analogWrite(E1, 0); 
